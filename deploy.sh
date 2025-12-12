256d194a2ba0863a081c8e56b0f8e84ff6676476
Nazar Kurii <nazar.nazar.kurii@gmail.com>
2025-12-12 04:23:05 +0100

    first commit

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# docker stop $(docker ps -aq)
+# docker rm $(docker ps -aq)
+# docker rmi -f $(docker images -aq)
+docker stop $(docker ps -q)
+docker rm -f $(docker ps --all -q)
+docker rmi -f $(docker images -q)
+docker compose pull
+docker compose up -d
+docker container prune -f   
+docker image prune -f
